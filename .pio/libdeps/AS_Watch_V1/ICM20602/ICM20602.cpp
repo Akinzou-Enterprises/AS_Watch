eda03c74bee435e60d5e51d10b0030cde52ab574
Wiktor Jn <wiktorjn@gmail.com>
2021-05-03 00:41:08 +0200

    CreatingLibForICM20602

---
+++ .pio/libdeps/AS_Watch_V1/ICM20602/ICM20602.cpp
@@ -0,0 +1,33 @@
+#include <ICM20602.h>
+
+ICM20602::ICM20602(bool Addr)
+{ 
+    if(Addr)
+    {
+        Addr = 0x69;
+    }
+    
+    else
+    {
+        Addr = 0x68;
+    }
+}
+
+void ICM20602::write8(byte reg, byte value) 
+{
+  Wire.beginTransmission((uint8_t)MAX31341Adress);
+  Wire.write((uint8_t)reg);
+  Wire.write((uint8_t)value);
+  Wire.endTransmission();
+}
+
+uint8_t ICM20602::read8(byte reg) 
+{
+    uint8_t value;
+    Wire.beginTransmission((uint8_t)MAX31341Adress);
+    Wire.write((uint8_t)reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom((uint8_t)MAX31341Adress, (byte)1);
+    value = Wire.read();
+    return value;
+}
